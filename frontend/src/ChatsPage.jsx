0ec4e5983affd2cf9bc98da30081ead5cee24232
Isuru Bimsara <bimsaragunasekara@gmail.com>
2023-05-17 11:34:50 +0530

    17051

--- frontend/src/ChatsPage.jsx
+++ frontend/src/ChatsPage.jsx
@@ -5,9 +5,10 @@ const ChatsPage = (props) => {
   return (
     <div className="background">
       <PrettyChatWindow
-        projectID='efa4b673-13c1-4ddf-b5bf-cd00510ce165'
+        projectID="efa4b673-13c1-4ddf-b5bf-cd00510ce165"
         username={props.user.username}
         secret={props.user.secret}
+        private_key='e5374696-36ad-4ca8-8a10-52a9e12a2fc3'
       />
     </div>
   );
